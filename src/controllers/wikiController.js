118e993c8f14fdd733ce1459b1433ea7030d49f0
Br <br@Brs-MacBook-Pro.local>
2019-06-03 21:31:43 -0700

    fixed signup inputs

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -64,15 +64,14 @@ module.exports = {
 
     edit(req, res, next) {
 
-        wikiQueries.getWiki(req.params.id, (err, result) => {
-            wiki = result["wiki"];
-            collaborators = result["collaborators"];
-            if (err) {
+        wikiQueries.getWiki(req.params.id, (err, wiki) => {
+
+            if (err || wiki == null) {
                 res.redirect(404, "/");
             } else {
-                const authorized = new Authorizer(req.user, wiki, collaborators).edit();
+                const authorized = new Authorizer(req.user, wiki).edit();
                 if (authorized) {
-                    res.render("wikis/edit", { wiki, collaborators });
+                    res.render("wikis/edit", { wiki });
                 } else {
                     req.flash("notice", "You are not authorized to do that.");
                     res.redirect(`/wikis/${req.params.id}`);
